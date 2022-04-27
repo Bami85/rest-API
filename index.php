7405385a540d3cd3c0dd7dc32100f9f5428962c8
Bami85 <91563447+Bami85@users.noreply.github.com>
2022-04-28 00:57:38 +0200

    restAPI updatering
    klart med jsonfiler, order,producter,post, media, och laddat upp sccreenshots.

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>MySite</title>
+</head>
+
+</html>
+   
+<body>
+  <header>
+    <div class="header-tekst">
+      <h1>
+       Rest-API
+      </h1>
+    
+      <h2>
+       Medieinstitutet.se
+      </h2>
+    </div>
+    
+    <div class="header-meny">
+      <a href="./products.php">Products</a><a href="./posts.php">Posts</a><a href="./orders.php">Orders</a><a href="./media.php">Media</a>
+    </div>
+  </header>
+</body>
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Roboto:500,700" rel="stylesheet">
+
+<img style="width: 80%; height:30%; margin:120px;" src="./img/Rest-API.jpg">
+
+</html>
